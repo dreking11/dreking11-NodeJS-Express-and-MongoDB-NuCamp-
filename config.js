28ea22745d119eb07cb5d4f916412fb60cd4a3bb
Dre King <dreking11@gmail.com>
2020-12-11 11:25:59 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '295283535231430',
+        clientSecret: 'c8cb2e08f60a5568a76709365c8a6b79'
+    }
 }
